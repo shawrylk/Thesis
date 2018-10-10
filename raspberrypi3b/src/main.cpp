c6f2278f77be07e07bd82522a014946d5e94ec97
shawrylk <shawry.lk@gmail.com>
2018-10-10 12:35:18 +0700

    update script to install opencv automatically

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -1,30 +1,95 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+#include <thread>
 #include <iostream>
-
-using namespace cv;
+#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <ctime>
+#include <unistd.h>
 using namespace std;
+using namespace cv;
 
-int main( int argc, char** argv )
+void captureVideoStream(void);
+void processFrame(void);
+static UMat frame;
+static VideoCapture cap(0);
+int main( int, char** ) 
 {
-    if( argc != 2)
-    {
-     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
-     return -1;
-    }
 
-    UMat image;
-    imread(argv[1], CV_LOAD_IMAGE_COLOR).copyto(image);   // Read the file
+	// static int fps;
+	// VideoCapture cap(0); // open the default camera
+	// cap.set(CAP_PROP_FRAME_WIDTH, 320);
+	// cap.set(CAP_PROP_FRAME_HEIGHT, 320);
+	// cap.set(CV_CAP_PROP_FPS, 60);
+	// if (!cap.isOpened())  // check if we succeeded
+	// 	return -1;
+	// UMat edges;
+	// UMat frame;
+	// namedWindow("edges", 1);
+	// std::time_t start = std::time(0);
+	// for (int i = 0; i <120; i++)
+	// {
+	// 	cap >> frame; // get a new frame from camera
+	// 	fps++;
+	// 	cvtColor(frame, edges, COLOR_BGR2GRAY);
+	// 	GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+	// 	Canny(edges, edges, 0, 30, 3);
+	// 	imshow("edges", edges);
+	// 	waitKey(1);
+		
+	// }
+	// std::time_t end = std::time(0);
+	// std::cout << "fps " << 120/(end - start) << std::endl;
+	// waitKey(5);
+	std::thread first(captureVideoStream);
+	std::thread second(processFrame);
+	std::cout << "Running\n";
+	first.join();
+	second.join();
+	return 0;
+}
 
-    if(! image.data )                              // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl ;
-        return -1;
-    }
-
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", image );                // Show our image inside it.
-
-    waitKey(0);             // Wait for a keystroke in the window
-    return 0;
+void captureVideoStream()
+{
+	
+	 // open the default camera
+	cap.set(CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CAP_PROP_FRAME_HEIGHT, 320);
+	cap.set(CV_CAP_PROP_FPS, 200);
+	//cap.set(CV_CAP_PROP_CONVERT_RGB , false);
+	if (cap.isOpened())
+			std::cout << "cant open cam\n";
+	while (1)
+	{
+		cap.read(frame);
+		waitKey(10);
+	}
+}
+void processFrame()
+{
+	UMat edges;
+	sleep(1);
+	int count = 0;
+	std::time_t start, end;
+	while(1)
+	{
+		if (frame.empty())
+			continue;
+		if (count == 0)
+			std::time(&start);
+		count++;
+		cvtColor(frame, edges, COLOR_BGR2GRAY);
+		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+		Canny(edges, edges, 0, 30, 3);
+		imshow("edges", edges);
+		waitKey(1);
+		
+		if (count == 1200)
+		{
+			std::time(&end);
+			std::cout << "fps " << 120.0 / difftime(end, start) << std::endl;
+			count = 0;
+		}
+		
+	}
 }
