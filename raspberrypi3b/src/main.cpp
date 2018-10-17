deec0abd3693ed6dd06aaca41520b5e46ff662ee
shawrylk <shawry.lk@gmail.com>
2018-10-17 14:29:18 +0700

    multithread

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -115,7 +115,7 @@ void cvtColorFrame(void)
         count++;
         sem_wait(&semCaptureFrameCplt);     
         cvtColor(frame,HSV,COLOR_BGR2HSV);
-		
+		inRange(HSV,Scalar(H_MIN,S_MIN,V_MIN),Scalar(H_MAX,S_MAX,V_MAX),thresh);
         sem_post(&semCvtColorFrameCplt);
         if (count == 1000)
         {
@@ -140,7 +140,7 @@ void threshFrame(void)
         if (count == 0)
             start = std::chrono::high_resolution_clock::now();
         count++;
-        inRange(HSV,Scalar(H_MIN,S_MIN,V_MIN),Scalar(H_MAX,S_MAX,V_MAX),thresh);
+        //inRange(HSV,Scalar(H_MIN,S_MIN,V_MIN),Scalar(H_MAX,S_MAX,V_MAX),thresh);
         sem_post(&semThreshFrameCplt);
         if (count == 1000)
         {
