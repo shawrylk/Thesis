4c925396bdc5b25bc5f91fee1103ec834f979bea
shawrylk <shawry.lk@gmail.com>
2018-10-18 15:44:36 +0700

    test ca,

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -321,7 +321,7 @@ void trackingObject(void)
 //         }
         Rect2d bbox(x - RECT_SIZE/2, y - RECT_SIZE/2, RECT_SIZE, RECT_SIZE); 
         rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 ); 
-        imshow("frame", thresh);
+        imshow("frame", frame);
         waitKey(1);
         // sem_post(&semTrackingObjectCplt);
         if (count == 1000)
