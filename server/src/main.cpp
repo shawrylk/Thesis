3914fdee2a77201f5f2dd276d7c9f3b301c6abd1
shawrylk <shawry.lk@gmail.com>
2018-10-23 16:53:59 +0700

    add comments to test flow code

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -151,7 +151,7 @@ main (int argc, char *argv[])
           }
           catch (std::exception e)
           {
-            std::cout << "recv fails: " + e.message() << "\n";
+            std::cout << "exception occurrs << "\n";
           }
           do {
             rc = send(new_sd, buffer, len, 0);
