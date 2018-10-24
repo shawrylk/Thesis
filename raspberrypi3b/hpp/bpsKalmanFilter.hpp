573b2f9ee73d915fce0aca9e3a134cdac614bfe5
shawrylk <shawry.lk@gmail.com>
2018-10-24 17:12:18 +0700

    Kalmanfilter

--- raspberrypi3b/hpp/bpsKalmanFilter.hpp
+++ raspberrypi3b/hpp/bpsKalmanFilter.hpp
@@ -1,3 +1,6 @@
+
+#pragma once
+
 class KalmanFilter
 {
     private:
@@ -9,4 +12,4 @@ class KalmanFilter
         KalmanFilter(float EST1, float Eest1, float Emeas)
             : EST1(EST1), Eest1(Eest1), Emeas(Emeas){};
         float predict(float MEA);
-}
+};
