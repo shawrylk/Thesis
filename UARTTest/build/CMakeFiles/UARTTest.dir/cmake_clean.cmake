0fe8e930c3155e88758158c5661432310f0cee07
shawrylk <shawry.lk@gmail.com>
2018-10-27 22:10:54 +0700

    unix serial

---
+++ UARTTest/build/CMakeFiles/UARTTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UARTTest.dir/src/main.cpp.o"
+  "../bin/UARTTest.pdb"
+  "../bin/UARTTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UARTTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- UARTTest/src/main.cpp
+++ UARTTest/src/main.cpp
@@ -1,53 +1,50 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <wiringPi.h>
-#include <wiringSerial.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 #include <thread>
-int send(int);
-int recv(int);
+#include <iostream>
+int msend(int);
+int mrecv(int);
 int fdes;
+
 int main()
 {
-	if((fdes= serialOpen ("/dev/serial0", 9600)) < 0 )
+	fdes = open("/dev/serial0", O_RDWR | O_NONBLOCK);
+	if (fdes < 0 )
     {
         return -1;
 	}
-  std::thread thread1(send,fdes);
-  std::thread thread2(recv,fdes);
+  std::thread thread1(msend,fdes);
+  std::thread thread2(mrecv,fdes);
   thread1.join();
   thread2.join();
       return 0;
 
 }
 
-int send(int fdes) {
+int msend(int fdes) {
  
  
 	printf("Raspberry's sending : \n");
  
 	while(1) {
-		serialPuts(fdes, "hello");
-		serialFlush(fdes);
-		//printf("%s\n", "hello");
-		fflush(stdout);
-		delay(1000);
+		write(fdes, "hello", 5);
+		sleep(1);
 	}
 	return 0;
 }
 
-int recv(int fdes) {
+int mrecv(int fdes) {
  
-	char c;
+	char* buff = new char[10];
 	printf("Raspberry's receiving : \n");
  
 	while(1) {
-			do{
-				c = serialGetchar(fdes);
-				printf("%c",c);
-				fflush (stdout);
-			}while(serialDataAvail(fdes));
+			read(fdes, buff, 5);
+			std::cout << buff << std::endl;
 		}
 	
 	return 0;
