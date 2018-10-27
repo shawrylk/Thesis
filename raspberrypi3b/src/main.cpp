cc2924dee9e0c5e1328bb80aa3feb0d946b939de
shawrylk <shawry.lk@gmail.com>
2018-10-28 00:59:06 +0700

    test UART

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -211,7 +211,7 @@ int sendFunc (char *sendData, int sendLen)
     bpsPointTypeDef *data = (bpsPointTypeDef*)sendData;
     data->setpointCoordinate[BPS_X_AXIS] = (int)KF.predict(STMData.ballCoordinate[BPS_X_AXIS]);
     data->setpointCoordinate[BPS_Y_AXIS] = (int)KF.predict(STMData.ballCoordinate[BPS_Y_AXIS]);
-    std::cout << "command: " << STMData.command << std::end;
+    std::cout << "command: " << STMData.command << std::endl;
     bpsUARTSendData(&STMData, sizeof(bpsUARTSendDataTypeDef));
     sem_post(&semSendDataCplt);
 }
