85e1719da24bf439865e730afa3fcf06e384d223
shawrylk <shawry.lk@gmail.com>
2018-10-28 11:15:53 +0700

    test UART

--- UARTTest/src/main.cpp
+++ UARTTest/src/main.cpp
@@ -6,14 +6,16 @@
 #include <termios.h>
 #include <thread>
 #include <iostream>
-
+#include <termios.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 int msend(int);
 int mrecv(int);
 int fdes;
 
 int main()
 {
-	fdes = open("/dev/serial0", O_RDWR | O_NONBLOCK);
+	fdes = open("/dev/serial0", O_RDWR);
 	if (fdes < 0 )
     {
         return -1;
@@ -32,7 +34,7 @@ int msend(int fdes) {
 	printf("Raspberry's sending : \n");
  
 	while(1) {
-		write(fdes, "hello", 5);
+		send(fdes, "hello", 5, MSG_DONTWAIT );
 		sleep(1);
 	}
 	return 0;
@@ -44,7 +46,7 @@ int mrecv(int fdes) {
 	printf("Raspberry's receiving : \n");
  
 	while(1) {
-			read(fdes, buff, 5);
+			recv(fdes, buff, 5, 0);
 			std::cout << buff << std::endl;
 		}
 	
