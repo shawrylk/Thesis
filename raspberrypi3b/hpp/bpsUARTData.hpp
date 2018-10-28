32c88f9f965425189bcbcccbc70ccdcc65e8e132
shawrylk <shawry.lk@gmail.com>
2018-10-28 13:56:37 +0700

    test UART

--- raspberrypi3b/hpp/bpsUARTData.hpp
+++ raspberrypi3b/hpp/bpsUARTData.hpp
@@ -8,10 +8,8 @@
 #include <errno.h>
 #include <unistd.h>
 #include <iostream>
-#include <fcntl.h>
-// #include <termios.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
 #define MIN_PWM_DUTY				0
 #define	MAX_PWM_DUTY				9599
