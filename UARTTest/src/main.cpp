97ebc7d9cb652ded589e3c4ecb6f2459fd65fc38
shawrylk <shawry.lk@gmail.com>
2018-10-28 11:17:06 +0700

    test UART

--- UARTTest/src/main.cpp
+++ UARTTest/src/main.cpp
@@ -34,7 +34,7 @@ int msend(int fdes) {
 	printf("Raspberry's sending : \n");
  
 	while(1) {
-		send(fdes, "hello", 5, MSG_DONTWAIT );
+		send(fdes, "hello", 5, 0 );
 		sleep(1);
 	}
 	return 0;
@@ -47,7 +47,7 @@ int mrecv(int fdes) {
  
 	while(1) {
 			recv(fdes, buff, 5, 0);
-			std::cout << buff << std::endl;
+			std::cout << buff[0] << buff[1] << buff[2] << buff[3] << buff[4] << std::endl;
 		}
 	
 	return 0;
