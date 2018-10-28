d60fedba1d6eda4fbea1b18cf9578dbdbeb00776
shawrylk <shawry.lk@gmail.com>
2018-10-28 12:39:43 +0700

    test UART

--- UARTTest/src/main.cpp
+++ UARTTest/src/main.cpp
@@ -27,16 +27,16 @@ int send(int fdes) {
 	int16_t ball[10];
 	int i = 0;
 	char *buff = new char[20];
-	ball[0] = 1;
-	ball[1] = 2;
-	ball[2] = 3;
-	ball[3] = 4;
-	ball[4] = 5;
-	ball[5] = 6;
-	ball[6] = 7;
-	ball[7] = 8;
-	ball[8] = 9;
-	ball[9] = 10;
+	ball[0] = 0x01;
+	ball[1] = 0x02;
+	ball[2] = 0x03;
+	ball[3] = 0x04;
+	ball[4] = 0x05;
+	ball[5] = 0x06;
+	ball[6] = 0x07;
+	ball[7] = 0x08;
+	ball[8] = 0x09;
+	ball[9] = 0x0A;
 	memcpy(buff,(char*)&ball, sizeof(int16_t) * 10);
 	printf("Raspberry's sending : \n");
  
@@ -75,9 +75,10 @@ int recv(int fdes) {
 				i = 0;
 				memcpy(ball, buff, 20);
 				printf("number: %d\n", i);
-				
+				for (int j =0; j < 20; j++)
+					printf("%x ",buff[j]);
 				for (int j =0; j < 10; j++)
-					printf("%d ",ball[j]);
+					printf("%x ",ball[j]);
 				printf("\n");
 				fflush(stdout);
 				memset(buff,0,20);
