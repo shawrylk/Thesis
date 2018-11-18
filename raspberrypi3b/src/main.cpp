23ec4a740720e56b6ad918cb2a9c4e57c63f270b
shawrylk <shawry.lk@gmail.com>
2018-11-18 16:43:42 +0700

    trackbar to rasppberry

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -268,6 +268,7 @@ void showImage(void)
     C=int(Contrast*100);
     S=int(Saturation*100);
     G=int(Gain*100);
+    T=int(80*255);
     createTrackbar( "Brightness",winName, &B, 100, onTrackbarChanged );
     createTrackbar( "Contrast",winName, &C, 100,onTrackbarChanged );
     createTrackbar( "Saturation",winName, &S, 100,onTrackbarChanged);
