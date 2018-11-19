a4afb9b296b5709357bbb989e2cf7a81814e4b95
shawrylk <shawry.lk@gmail.com>
2018-11-19 20:45:48 +0700

    optimise code

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -106,7 +106,6 @@ void captureFrame(void)
         //*******************************//
         video.read(frame); 
         sem_post(&semCaptureFrameCplt);
-        std::cout << "size : " << sizeof(bpsUARTSendDataTypeDef) << std::endl;
         //*******************************//
         if (count == 1000)
         {
