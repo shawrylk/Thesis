3012fbeeb7d505563aaff41abffb7bd7ff90efd2
shawrylk <shawry.lk@gmail.com>
2018-11-23 16:11:54 +0700

    test 2 thres

--- raspberrypi3b/src/main.cpp
+++ raspberrypi3b/src/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
         ( std::ostringstream() << std::dec << x ) ).str()
 #define FRAME_WIDTH     480
 #define FRAME_HEIGHT    480
-#define FPS             90
+#define FPS             30
 #define MAX_NUM_OBJECTS 10
 #define MIN_OBJECT_AREA 80*80
 #define MAX_OBJECT_AREA 120*120
